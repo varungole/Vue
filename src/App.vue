b9071a414ff1ac32bf3d2df77b4b7aed8c44c093
Varun Gole <varungole1999@gmail.com>
2024-06-30 09:07:01 -0400

    ashish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<p>Simple text</p>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            activePage: 0,
+            pages: [
+                {
+                    link: {text: 'Home', url: 'index.html'},
+                    pageTitle: 'Home Page'
+                },
+                 {
+                    link: {text: 'About', url: 'about.html'},
+                    pageTitle: 'About Page'
+                }
+
+            ]
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
